41b316b92bb8ab1b620641ea17d04804ea2d4d7e
janhuang <384099566@qq.com>
2015-03-30 23:20:23 +0800

    调整文件

---
+++ src/Welcome/Commands/DemoCommands.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: janhuang
+ * Date: 15/3/30
+ * Time: 下午11:16
+ * Github: https://www.github.com/janhuang 
+ * Coding: https://www.coding.net/janhuang
+ * SegmentFault: http://segmentfault.com/u/janhuang
+ * Blog: http://segmentfault.com/blog/janhuang
+ * Gmail: bboyjanhuang@gmail.com
+ */
+
+namespace Welcome\Commands;
+
+use Dobee\Console\Commands\Command;
+use Dobee\Console\Format\Input;
+use Dobee\Console\Format\Output;
+
+class DemoCommands extends Command
+{
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'demo:command';
+    }
+
+    /**
+     * @return void|$this
+     */
+    public function configure()
+    {
+        // TODO: Implement configure() method.
+    }
+
+    /**
+     * @param Input  $input
+     * @param Output $output
+     * @return void
+     */
+    public function execute(Input $input, Output $output)
+    {
+        // TODO: Implement execute() method.
+    }
+}
