b9d86f9ce39fc52a1c502a3e8a10fbb76c03567a
JanHuang <bboyjanhuang@gmail.com>
2017-03-12 14:50:38 +0800

    change seed command

--- src/Console/SeedRun.php
+++ src/Console/SeedRun.php
@@ -11,9 +11,22 @@ namespace FastD\Console;
 
 
 use Phinx\Config\Config as MConfig;
+use Phinx\Console\Command\Migrate;
+use Phinx\Migration\AbstractMigration;
+use Phinx\Migration\Manager;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
-class SeedRun extends \Phinx\Console\Command\SeedRun
+class SeedRun extends Migrate
 {
+    protected function loadManager(InputInterface $input, OutputInterface $output)
+    {
+        if (null === $this->getManager()) {
+            $manager = new Migration($this->getConfig(), $input, $output);
+            $this->setManager($manager);
+        }
+    }
+
     public function configure()
     {
         parent::configure();
@@ -39,5 +52,51 @@ class SeedRun extends \Phinx\Console\Command\SeedRun
                 )
             )
         )));
+    }
+}
+
+class Migration extends Manager
+{
+    /**
+     * Gets an array of the database migrations, indexed by migration name (aka creation time) and sorted in ascending
+     * order
+     *
+     * @throws \InvalidArgumentException
+     * @return AbstractMigration[]
+     */
+    public function getMigrations()
+    {
+        if (null === $this->migrations) {
+            $phpFiles = $this->getMigrationFiles();
+
+            // filter the files to only get the ones that match our naming scheme
+            $fileNames = array();
+            /** @var AbstractMigration[] $versions */
+            $versions = array();
+
+
+
+            foreach ($phpFiles as $filePath) {
+                $class = pathinfo($filePath, PATHINFO_FILENAME);
+                $fileNames[$class] = basename($filePath);
+                require_once $filePath;
+                $version = date('YmdHis');
+                $migration = new $class($version, $this->getInput(), $this->getOutput());
+
+                if (!($migration instanceof AbstractMigration)) {
+                    throw new \InvalidArgumentException(sprintf(
+                        'The class "%s" in file "%s" must extend \Phinx\Migration\AbstractMigration',
+                        $class,
+                        $filePath
+                    ));
+                }
+                $versions[$version] = $migration;
+            }
+
+            ksort($versions);
+            $this->setMigrations($versions);
+        }
+
+        return $this->migrations;
     }
 }
---
+++ tests/database/seeds/Dem.php
@@ -0,0 +1,62 @@
+
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class Dem extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        if (!$this->hasTable('user_logins')) {
+            $table = $this->table('user_logins', ['id' => false]);
+            $table
+                ->addColumn('user_id', 'integer')
+                ->addColumn('created', 'datetime')
+                ->create()
+            ;
+        }
+    }
+
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        echo 'up';die;
+        $this->insert('user_logins', [
+            [
+                'user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ]
+        ]);
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+    
+    }
+}
