c4b9709165cab59dac42d63272d4f51be53fc53a
JanHuang <bboyjanhuang@gmail.com>
2017-03-17 17:51:06 +0800

    add config dump

--- src/Console/Config.php
+++ src/Console/Config.php
@@ -13,8 +13,10 @@ namespace FastD\Console;
 use FastD\Utils\FileObject;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * Class Config
@@ -25,19 +27,33 @@ class Config extends Command
     public function configure()
     {
         $this->setName('config:dump');
+        $this->addArgument('name', InputArgument::OPTIONAL, 'file name');
     }
 
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return mixed
+     */
     public function execute(InputInterface $input, OutputInterface $output)
     {
+        if ($input->getArgument('name')) {
+            $file = app()->getPath() . '/config/' . $input->getArgument('name') . '.php';
+            $config = load($file);
+            $output->writeln('file: ' . $file);
+            $output->writeln('<info>' . Yaml::dump($config) . '</info>');
+            return 0;
+        }
         $table = new Table($output);
         $rows = [];
         $table->setHeaders(array('File', 'Config', 'Owner', 'Modify At',));
 
         foreach (glob(app()->getPath() . '/config/*') as $file) {
             $file = new FileObject($file);
+            $config = load($file->getPathname());
             $rows[] = [
                 $file->getFilename(),
-                count(array_keys(load($file->getPathname()))) . ' Keys',
+                count(array_keys($config)) . ' Keys',
                 posix_getpwuid($file->getOwner())['name'],
                 date('Y-m-d H:i:s', $file->getMTime()),
             ];
