953c466b7e5f414bab8bc9e31c7f0f908f5a0ec3
JanHuang <bboyjanhuang@gmail.com>
2017-09-08 18:31:32 +0800

    fixed style

--- src/Console/Processor.php
+++ src/Console/Processor.php
@@ -1,123 +1,127 @@
 <?php
+/**
+ * @author    jan huang <bboyjanhuang@gmail.com>
+ * @copyright 2017
+ *
+ * @see      https://www.github.com/janhuang
+ * @see      http://www.fast-d.cn/
+ */
+
+namespace FastD\Console;
+
+use FastD\Swoole\Process;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class Process.
+ */
+class Processor extends Command
+{
     /**
-     * @author    jan huang <bboyjanhuang@gmail.com>
-     * @copyright 2017
-     *
-     * @see      https://www.github.com/janhuang
-     * @see      http://www.fast-d.cn/
+     * php bin/console process {name} {args} {options}
      */
-    
-    namespace FastD\Console;
-    
-    use FastD\Swoole\Process;
-    use Symfony\Component\Console\Command\Command;
-    use Symfony\Component\Console\Helper\Table;
-    use Symfony\Component\Console\Input\InputArgument;
-    use Symfony\Component\Console\Input\InputInterface;
-    use Symfony\Component\Console\Input\InputOption;
-    use Symfony\Component\Console\Output\OutputInterface;
-    
-    /**
-     * Class Process.
-     */
-    class Processor extends Command
+    protected function configure()
     {
-        /**
-         * php bin/console process {name} {args} {options}
-         */
-        protected function configure()
-        {
-            $this->setName('process');
-            $this->addArgument('process', InputArgument::OPTIONAL);
-            $this->addOption('pid', '-p', InputOption::VALUE_OPTIONAL, 'set process pid path.');
-            $this->addOption('name', null, InputOption::VALUE_OPTIONAL, 'set process name.', app()->getName());
-            $this->addOption('daemon', '-d', InputOption::VALUE_NONE, 'set process daemonize.');
-            $this->addOption('list', '-l', InputOption::VALUE_NONE, 'show all processes.');
-            $this->setDescription('Create new processor.');
-        }
-        
-        /**
-         * @param InputInterface  $input
-         * @param OutputInterface $output
-         *
-         * @return int
-         */
-        protected function execute(InputInterface $input, OutputInterface $output)
-        {
-            $process = $input->getArgument('process');
-            
-            if ($input->hasParameterOption(['--list', '-l']) || empty($process)) {
-                return $this->showProcesses($input, $output);
-            }
-            
-            $processes = config()->get('processes', []);
-            
-            if (!isset($processes[$process])) {
-                throw new \RuntimeException(sprintf('Process %s cannot found', $process));
-            }
-            
-            $processor = $processes[$process];
-            if (!class_exists($processor)) {
-                throw new \RuntimeException(sprintf('Class "%s" is not found.', $process));
-            }
-            $name = $input->getOption('name');
-            $process = new $processor($name);
-            if (!($process instanceof Process)) {
-                throw new \RuntimeException('Process must be instance of \FastD\Swoole\Process');
-            }
-            if ($input->hasParameterOption(['--daemon', '-d'])) {
-                $process->daemon();
-            }
-            
-            $path = $this->targetDirectory($input);
-            $file = $path.'/'.$name.'.pid';
-            
-            $pid = $process->start();
-            file_put_contents($file, $pid);
-            
-            $output->writeln(sprintf('Process %s is started, pid: %s', $name, $pid));
-            $output->writeln(sprintf('Pid file save is %s', $file));
-            
-            return $pid;
-        }
-        
-        /**
-         * @param InputInterface $input
-         * @return string
-         */
-        protected function targetDirectory(InputInterface $input)
-        {
-            $pid = $input->getParameterOption(['--path', '-p']);
-            
-            if (empty($pid)) {
-                $path = app()->getPath().'/runtime/process';
-            } else {
-                $path = dirname($pid);
-            }
-            if (!file_exists($path)) {
-                mkdir($path, true, 0755);
-            }
-            
-            return $path;
-        }
-        
-        /**
-         * @param InputInterface $input
-         * @param OutputInterface $output
-         * @return int
-         */
-        protected function showProcesses(InputInterface $input, OutputInterface $output)
-        {
-            $table = new Table($output);
-            $table->setHeaders(array('Process', 'Pid', 'Status', 'Start At', 'Runtime'));
-            $rows = [];
-            foreach (config()->get('processes', []) as $name => $processor) {
-                $rows[] = [
-                $name, '', '', '',
-                ];
-            }
-            $table->setRows($rows);
-            $table->render();
-            return 0;
-        }
+        $this->setName('process');
+        $this->addArgument('process', InputArgument::OPTIONAL);
+        $this->addOption('pid', '-p', InputOption::VALUE_OPTIONAL, 'set process pid path.');
+        $this->addOption('name', null, InputOption::VALUE_OPTIONAL, 'set process name.', app()->getName());
+        $this->addOption('daemon', '-d', InputOption::VALUE_NONE, 'set process daemonize.');
+        $this->addOption('list', '-l', InputOption::VALUE_NONE, 'show all processes.');
+        $this->setDescription('Create new processor.');
     }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $process = $input->getArgument('process');
+
+        if ($input->hasParameterOption(['--list', '-l']) || empty($process)) {
+            return $this->showProcesses($input, $output);
+        }
+
+        $processes = config()->get('processes', []);
+
+        if ( ! isset($processes[$process])) {
+            throw new \RuntimeException(sprintf('Process %s cannot found', $process));
+        }
+
+        $processor = $processes[$process];
+        if ( ! class_exists($processor)) {
+            throw new \RuntimeException(sprintf('Class "%s" is not found.', $process));
+        }
+        $name = $input->getOption('name');
+        $process = new $processor($name);
+        if ( ! ($process instanceof Process)) {
+            throw new \RuntimeException('Process must be instance of \FastD\Swoole\Process');
+        }
+        if ($input->hasParameterOption(['--daemon', '-d'])) {
+            $process->daemon();
+        }
+
+        $path = $this->targetDirectory($input);
+        $file = $path.'/'.$name.'.pid';
+
+        $pid = $process->start();
+        file_put_contents($file, $pid);
+
+        $output->writeln(sprintf('Process %s is started, pid: %s', $name, $pid));
+        $output->writeln(sprintf('Pid file save is %s', $file));
+
+        return $pid;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @return string
+     */
+    protected function targetDirectory(InputInterface $input)
+    {
+        $pid = $input->getParameterOption(['--path', '-p']);
+
+        if (empty($pid)) {
+            $path = app()->getPath().'/runtime/process';
+        } else {
+            $path = dirname($pid);
+        }
+        if ( ! file_exists($path)) {
+            mkdir($path, true, 0755);
+        }
+
+        return $path;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
+    protected function showProcesses(InputInterface $input, OutputInterface $output)
+    {
+        $table = new Table($output);
+        $table->setHeaders(array('Process', 'Pid', 'Status', 'Start At', 'Runtime'));
+        $rows = [];
+        foreach (config()->get('processes', []) as $name => $processor) {
+            $rows[] = [
+                $name,
+                '',
+                '',
+                '',
+            ];
+        }
+        $table->setRows($rows);
+        $table->render();
+
+        return 0;
+    }
+}
--- src/Process/Queue.php
+++ src/Process/Queue.php
@@ -1,36 +1,36 @@
 <?php
-    /**
-     * @author    jan huang <bboyjanhuang@gmail.com>
-     * @copyright 2017
-     *
-     * @see      https://www.github.com/janhuang
-     * @see      http://www.fast-d.cn/
-     */
-    
-    namespace FastD\Process;
-    
-    
-    use FastD\Server;
-    use FastD\Swoole\Queue as Q;
-    use swoole_process;
-    
-    /**
-     * Class Queue
-     * @package FastD\Process
-     */
-    class Queue extends Q
+/**
+ * @author    jan huang <bboyjanhuang@gmail.com>
+ * @copyright 2017
+ *
+ * @see      https://www.github.com/janhuang
+ * @see      http://www.fast-d.cn/
+ */
+
+namespace FastD\Process;
+
+
+use FastD\Server;
+use FastD\Swoole\Queue as Q;
+use swoole_process;
+
+/**
+ * Class Queue
+ * @package FastD\Process
+ */
+class Queue extends Q
+{
+    public function handle(swoole_process $process)
     {
-        public function handle(swoole_process $process)
-        {
-            try {
-                $server = server();
-            } catch (\Exception $e) {
-                //            $server = new Server();
-            }
-            
-            while (true) {
-                $recv = $process->pop();
-                $process->write($recv);
-            }
+        try {
+            $server = server();
+        } catch (\Exception $e) {
+            //            $server = new Server();
+        }
+
+        while (true) {
+            $recv = $process->pop();
+            $process->write($recv);
         }
     }
+}
