c577f54c9a8de3ba0734472db0555d8f02b272a1
janhuang <huangjiajun@linghit.com>
2017-09-14 12:13:51 +0800

    optimize log trace format

--- src/Logger/Formatter/StashFormatter.php
+++ src/Logger/Formatter/StashFormatter.php
@@ -20,4 +20,45 @@ class StashFormatter extends LogstashFormatter
     {
         parent::__construct(app()->getName(), get_local_ip(), null, null, self::V1);
     }
+
+    public function formatV1(array $record)
+    {
+        if (empty($record['datetime'])) {
+            $record['datetime'] = gmdate('c');
+        }
+        $message = array(
+            '@timestamp' => $record['datetime'],
+            '@version' => 1,
+            'host' => $this->systemName,
+        );
+        if (isset($record['message'])) {
+            $message['message'] = $record['message'];
+        }
+        if (isset($record['channel'])) {
+            $message['type'] = $record['channel'];
+            $message['channel'] = $record['channel'];
+        }
+        if (isset($record['level_name'])) {
+            $message['level'] = $record['level_name'];
+        }
+        if ($this->applicationName) {
+            $message['type'] = $this->applicationName;
+        }
+        if (!empty($record['extra'])) {
+            foreach ($record['extra'] as $key => $val) {
+                $message[$this->extraPrefix . $key] = $val;
+            }
+        }
+        if (!empty($record['context'])) {
+            foreach ($record['context'] as $key => $val) {
+                $message[$this->contextPrefix . $key] = $val;
+            }
+        }
+
+        if (isset($message['trace']) && is_array($message['trace'])) {
+            $message['trace'] = implode(PHP_EOL, $message['trace']);
+        }
+
+        return $message;
+    }
 }
