f7f81b4c2f6b93ec6af5b99817c10f5332ea3da6
JanHuang <bboyjanhuang@gmail.com>
2017-11-09 01:07:35 +0800

    add forge code

---
+++ register.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @author    jan huang <bboyjanhuang@gmail.com>
+ * @copyright 2017
+ *
+ * @link      https://www.github.com/janhuang
+ * @link      http://www.fast-d.cn/
+ */
---
+++ src/RegisterCenter.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author    jan huang <bboyjanhuang@gmail.com>
+ * @copyright 2017
+ *
+ * @link      https://www.github.com/janhuang
+ * @link      http://www.fast-d.cn/
+ */
+
+namespace FastD;
+
+
+use FastD\Servitization\ServiceRegister\RedisRegisterCenter;
+use FastD\Servitization\ServiceRegister\Register;
+
+class RegisterCenter
+{
+    public function __construct()
+    {
+        // $register = new Register(new RedisRegisterCenter());
+    }
+
+    public function start()
+    {
+
+    }
+}
