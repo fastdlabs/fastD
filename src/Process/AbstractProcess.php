8b30c77246e252fe63311cb8f474e301de80be9d
JanHuang <bboyjanhuang@gmail.com>
2017-11-24 19:39:34 +0800

    add process options

---
+++ src/Process/AbstractProcess.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @author    jan huang <bboyjanhuang@gmail.com>
+ * @copyright 2017
+ *
+ * @see      https://www.github.com/janhuang
+ * @see      http://www.fast-d.cn/
+ */
+
+namespace FastD\Process;
+
+
+use FastD\Swoole\Process;
+
+/**
+ * Class AbstractProcess
+ * @package FastD\Process
+ */
+abstract class AbstractProcess extends Process
+{
+    protected $options = [];
+
+    /**
+     * @param array $options
+     * @return $this
+     */
+    public function configure(array $options = [])
+    {
+        $this->options = $options;
+
+        return $this;
+    }
+}
