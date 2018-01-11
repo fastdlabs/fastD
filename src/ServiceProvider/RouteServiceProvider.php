b32a99ef535b8ff3a43a13da59eedd2f63b0cb35
JanHuang <bboyjanhuang@gmail.com>
2018-01-11 15:33:05 +0800

    compatible route namespace

--- src/ServiceProvider/RouteServiceProvider.php
+++ src/ServiceProvider/RouteServiceProvider.php
@@ -14,47 +14,6 @@ use FastD\Container\ServiceProviderInterface;
 use FastD\Routing\RouteCollection;
 use FastD\Routing\RouteDispatcher;
 
-class Router extends RouteCollection
-{
-    protected function concat($callback)
-    {
-        if (is_string($callback)) {
-            return '\\Controller\\'.$callback;
-        }
-
-        return $callback;
-    }
-
-    public function get($path, $callback, array $defaults = [])
-    {
-        return parent::get($path, $this->concat($callback), $defaults);
-    }
-
-    public function post($path, $callback, array $defaults = [])
-    {
-        return parent::post($path, $this->concat($callback), $defaults);
-    }
-
-    public function patch($path, $callback, array $defaults = [])
-    {
-        return parent::patch($path, $this->concat($callback), $defaults);
-    }
-
-    public function put($path, $callback, array $defaults = [])
-    {
-        return parent::put($path, $this->concat($callback), $defaults);
-    }
-
-    public function delete($path, $callback, array $defaults = [])
-    {
-        return parent::delete($path, $this->concat($callback), $defaults);
-    }
-
-    public function head($path, $callback, array $defaults = [])
-    {
-        return parent::head($path, $this->concat($callback), $defaults);
-    }
-}
 
 /**
  * Class RouteServiceProvider.
@@ -68,7 +27,7 @@ class RouteServiceProvider implements ServiceProviderInterface
      */
     public function register(Container $container)
     {
-        $router = new Router();
+        $router = new RouteCollection(config()->get('namespace', '\\Controller\\'));
         $dispatcher = new RouteDispatcher($router, $container['config']->get('middleware', []));
 
         $container->add('router', $router);
