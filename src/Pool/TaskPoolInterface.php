eadff39ecd5d0ad2c2bebcb1fabb8f999e117626
longxinhui <longxinhui.e@gmail.com>
2019-05-16 12:19:47 +0800

    add Pool
    增加2个Pool接口，用于在WorkerStart的时候，判断Pool需要在Worker使用还是Task使用

---
+++ src/Pool/TaskPoolInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace FastD\Pool;
+
+/**
+ * Interface TaskPoolInterface.
+ */
+interface TaskPoolInterface
+{
+    /**
+     * @return mixed
+     */
+    public function initPool();
+}
